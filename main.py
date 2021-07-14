e5c6fd93234241bbaabfd51af9e0aa7f18462bf8
ortariot <ortariot@gmail.com>
2021-07-15 04:34:52 +0700

    create repo

---
+++ main.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+# читаем адресную книгу в формате CSV в список contacts_list
+import csv
+
+with open("phonebook_raw.csv", encoding='utf-8') as f:
+  rows = csv.reader(f, delimiter=",")
+#   for line in rows:
+#       print(line)
+  contacts_list = list(rows)
+  pprint(contacts_list)
+#   print(rows)
+
+# # TODO 1: выполните пункты 1-3 ДЗ
+# # ваш код
+
+# # TODO 2: сохраните получившиеся данные в другой файл
+# # код для записи файла в формате CSV
+# with open("phonebook.csv", "w") as f:
+#   datawriter = csv.writer(f, delimiter=',')
+#   # Вместо contacts_list подставьте свой список
+#   datawriter.writerows(contacts_list)
